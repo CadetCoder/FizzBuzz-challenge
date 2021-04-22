2b69e01ebeeb3dfc51b636ffdc8d6d8c2db49c65
Darrel Toledo <darreltoledo@icloud.com>
2021-04-22 21:52:30 +0100

    Fizz and Buzz printed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 for (var i = 1; i < 100; i++) {
   if ( i % 3 === 0) {
-    console.log('Fizz')
+    console.log('Fizz');
+  }
+  else if ( i % 5 === 0) {
+    console.log('Buzz')
   }
   else {
-    //( i % 5 === 0) {
-      //console.log('Buzz')
       console.log(i);
-    //}
   }
 }
